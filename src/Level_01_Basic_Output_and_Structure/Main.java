26fc7abfb770cda6259ce14ddb3e6f6b794f75a3
Gary-Maddison <garyfmaddison@gmail.com>
2026-02-28 13:09:59 +0000

    Initial commit

---
+++ src/Level_01_Basic_Output_and_Structure/Main.java
@@ -0,0 +1,9 @@
+package Level_01_Basic_Output_and_Structure;
+
+public class Main {
+
+        void main(String[] args) {
+            System.out.println("Java Level 01: System Ready!");
+    }
+}
+
