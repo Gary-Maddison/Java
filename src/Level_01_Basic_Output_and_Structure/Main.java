04c93302af634ef47b85cbf9df27891eb7a6c0a6
Gary-Maddison <garyfmaddison@gmail.com>
2026-02-28 13:19:44 +0000

    Commit

--- src/Level_01_Basic_Output_and_Structure/Main.java
+++ src/Level_01_Basic_Output_and_Structure/Main.java
@@ -3,7 +3,7 @@ package Level_01_Basic_Output_and_Structure;
 public class Main {
 
         void main(String[] args) {
-            System.out.println("Java Level 01: System Ready!!!");
+            System.out.println("Java Level 01: System Ready!!");
     }
 }
 
