caccdcfe7b7db8f5017e75e3891fecaac00dcb9a
Gary-Maddisongit config --global user.email <garyfmaddison@gmail.com>
2026-02-28 14:15:09 +0000

    Commit

--- src/Level_01_Basic_Output_and_Structure/Main.java
+++ src/Level_01_Basic_Output_and_Structure/Main.java
@@ -2,8 +2,9 @@ package Level_01_Basic_Output_and_Structure;
 
 public class Main {
 
-        void main(String[] args) {
-            System.out.println("Java Level 01: System Ready!!");
+    void main() {
+
+        System.out.println("Java Level 01: System Ready!!!!!!");
     }
 }
 
