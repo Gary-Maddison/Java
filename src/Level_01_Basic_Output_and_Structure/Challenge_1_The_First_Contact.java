01941de81e3006d1f03024c779879c33117ad333
Gary-Maddison <garyfmaddison@gmail.com>
2026-03-11 16:36:38 +0000

    Message Sent

---
+++ src/Level_01_Basic_Output_and_Structure/Challenge_1_The_First_Contact.java
@@ -0,0 +1,8 @@
+package Level_01_Basic_Output_and_Structure;
+
+public class Challenge_1_The_First_Contact {
+
+    void main() {
+        System.out.println("I am learning to program in Java!!!");
+    }
+}
--- src/Level_02_Java_Fundamentals_Variables_Text_Processing_and_Input_Mastery/Challenge_10_The_Clone_Check.java
+++ src/Level_02_Java_Fundamentals_Variables_Text_Processing_and_Input_Mastery/Challenge_10_The_Clone_Check.java
@@ -1,8 +1,21 @@
+/*
+Create an identifier pointing to a specific text message. Create a second identifier and point it to the exact
+same text message using the first identifier. Modify the second identifier by attaching a new word to the end of it.
+Output both identifiers to the screen to prove whether altering the second one affected the original first one.
+ */
+
 package Level_02_Java_Fundamentals_Variables_Text_Processing_and_Input_Mastery;
 
 public class Challenge_10_The_Clone_Check {
 
     void main() {
+
+        String firstWord = "Hello", secondWord = firstWord;
+
+        secondWord = secondWord + " World";
+
+        System.out.println(firstWord);
+        System.out.println(secondWord);
 
     }
 }
